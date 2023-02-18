0f5865d3ec39c8f9511dc1257b37cd4498cce13f
sans-mathuria <sanskritimathuria@gmail.com>
2023-02-18 12:02:24 +0530

    Pipeline

---
+++ new.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world"<<endl;
+    cout<<"This is a pipeline job"<<endl;
+    cout<<"PES1UG20CS386 successful pipeline build";
+    return 0;
+}
