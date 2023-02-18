fb7eb1c85d9fbdd942eb3eaa0e1c2f3d78388f68
sans-mathuria <sanskritimathuria@gmail.com>
2023-02-18 11:43:54 +0530

    Second commit

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I, PES1UG20CS386, have successfully built and run\n";
+  cout << "I, PES1UG20CS386(Sanskriti Mathuria), have successfully built and run\n";
   return 0;
 }
